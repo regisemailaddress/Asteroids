2b982501e09a9916fab781f1bad86ea533305b6f
root <root@DESKTOP-QPET326.localdomain>
2025-11-08 08:31:41 +0000

    Started GUI screen

---
+++ main.py
@@ -0,0 +1,21 @@
+import pygame
+from constants import *
+from logger import log_state
+
+def main():
+    pygame.init()
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    print("Starting Asteroids!")
+    print(f"Screen width: {SCREEN_WIDTH}")
+    print(f"Screen height: {SCREEN_HEIGHT}")
+    while True:
+        log_state()
+	for event in pygame.event.get():
+    	    if event.type == pygame.QUIT:
+                return
+        screen.fill("black", rect=None, special_flags=0)
+    pygame.display.flip() 
+
+
+if __name__ == "__main__":
+    main()
