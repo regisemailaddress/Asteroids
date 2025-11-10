d0de00298cbd211fb89c6fda408470cf2be6eb54
regisemailaddress <regisemailaddress@gmail.com>
2025-11-10 11:14:29 +0000

    Added rotation to player triangle

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import pygame
 from constants import *
 from logger import log_state
+from player import *
 
 def main():
     pygame.init()
@@ -8,12 +9,18 @@ def main():
     print("Starting Asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
+    player= Player ((SCREEN_WIDTH/2),(SCREEN_HEIGHT/2))
+    clock= pygame.time.Clock()
+    dt=0
     while True:
         log_state()
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+        dt= (clock.tick(60))/1000
         screen.fill("black", rect=None, special_flags=0)
+        player.draw(screen)
+        player.update(dt)
         pygame.display.flip()
 
 
